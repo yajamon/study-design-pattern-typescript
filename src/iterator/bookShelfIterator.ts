8ed1664c36c4b9ee865ffff3ff45eeb8c125e9a3
yajamon <yajamon.tatsuki@gmail.com>
2017-10-17 20:18:19 +0900

    Edit class: BookShelfIterator
    Implement methods of Iterator : hasNext() next()

--- src/iterator/bookShelfIterator.ts
+++ src/iterator/bookShelfIterator.ts
@@ -4,4 +4,17 @@ export class BookShelfIterator implements Iterator {
     private index = 0;
     constructor(private bookShelf: BookShelf) {
     }
+
+    public hasNext(): boolean {
+        if (this.index < this.bookShelf.length) {
+            return true;
+        }
+        return false;
+    }
+
+    public next(): any {
+        const book = this.bookShelf.bookAt(this.index);
+        this.index++;
+        return book;
+    }
 }
