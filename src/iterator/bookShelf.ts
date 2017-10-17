edda067ef09dd585d389541cf989b5826990c3e5
yajamon <yajamon.tatsuki@gmail.com>
2017-10-17 19:44:21 +0900

    Edit class: BookShelf
    Add method: appendBook

--- src/iterator/bookShelf.ts
+++ src/iterator/bookShelf.ts
@@ -2,4 +2,10 @@ import { Aggregate } from "./aggregate";
 import { Book } from "./book";
 export class BookShelf implements Aggregate {
     private books: Book[] = [];
+    private last: number = 0;
+
+    public appendBook(book: Book) {
+        this.books.push(book);
+        this.last = this.books.length - 1;
+    }
 }
