30f80ebd1463b6e11bab36fcce30b8e0f2db58dd
yajamon <yajamon.tatsuki@gmail.com>
2017-10-17 20:19:42 +0900

    Edit runner

--- src/iterator/index.ts
+++ src/iterator/index.ts
@@ -4,3 +4,10 @@ import { BookShelf } from "./bookShelf";
 const shelf = new BookShelf();
 
 shelf.appendBook(new Book("A first commit"));
+
+const iterator = shelf.iterator();
+while (iterator.hasNext()) {
+    const book = iterator.next();
+    console.log("book name is", book.name);
+
+}
