f65f5d9af03dc2df722cd3ad1e7572f62fd1bb07
yajamon <yajamon.tatsuki@gmail.com>
2017-10-18 11:51:21 +0900

    Change Type of Print: interface to abscract class

--- src/adapter/composit/print.ts
+++ src/adapter/composit/print.ts
@@ -1,4 +1,4 @@
-export interface Print {
-    printWeak: () => string;   // ()で文字列を包む
-    printStrong: () => string; // **で文字列を包む
+export abstract class Print {
+    abstract public printWeak(): string;   // ()で文字列を包む
+    abstract public printStrong(): string; // **で文字列を包む
 }
