faea4107226dc3b65173a014d2b30de357974c30
yajamon <yajamon.tatsuki@gmail.com>
2017-10-19 14:41:31 +0900

    Add class: CharDisplay

---
+++ src/templateMethod/charDisplay.ts
@@ -0,0 +1,17 @@
+import { Display } from "./display";
+
+export class CharDisplay extends Display {
+    private ch: string;
+    constructor(text: string) {
+        super();
+        if (text.length > 1) {
+            this.ch = text[0];
+            return;
+        }
+        this.ch = text;
+    }
+
+    public open() {
+        console.log("<<");
+    }
+}
