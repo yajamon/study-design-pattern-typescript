5c99eaff4de3abcbdb9829042beae5aa105ea408
yajamon <yajamon.tatsuki@gmail.com>
2017-10-21 02:50:21 +0900

    Implement Singleton

--- src/singleton/singleton.ts
+++ src/singleton/singleton.ts
@@ -1,2 +1,9 @@
 export class Singleton {
+    public static sharedInstance(): Singleton {
+        return this._instance;
+    }
+    private static _instance: Singleton = new Singleton();
+    private constructor() {
+        console.log("インスタンスを生成しました。");
+    }
 }
