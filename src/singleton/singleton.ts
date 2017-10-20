dd3a96a3b39c447d5309185ef5270dce60b64c72
yajamon <yajamon.tatsuki@gmail.com>
2017-10-21 03:28:26 +0900

    Edit Singleton

--- src/singleton/singleton.ts
+++ src/singleton/singleton.ts
@@ -1,8 +1,11 @@
 export class Singleton {
     public static sharedInstance(): Singleton {
+        if (!this._instance) {
+            this._instance = new Singleton();
+        }
         return this._instance;
     }
-    private static _instance: Singleton = new Singleton();
+    private static _instance: Singleton;
     private constructor() {
         console.log("インスタンスを生成しました。");
     }
