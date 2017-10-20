e852a148eaae0d5ac982a0071274da5d8221ed55
yajamon <yajamon.tatsuki@gmail.com>
2017-10-21 00:16:19 +0900

    Add class: IDCard

---
+++ src/factoryMethod/idCard.ts
@@ -0,0 +1,12 @@
+import * as process from "process";
+import { Product } from "./product";
+
+export class IDCard extends Product {
+    constructor(public readonly owner: string) {
+        super();
+        process.stdout.write(`${this.owner}のカードを使います。\n`);
+    }
+    public use() {
+        process.stdout.write(`${this.owner}のカードを使います。\n`);
+    }
+}
