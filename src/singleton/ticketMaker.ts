38959c81c4532cd200c124bbd35e63980b844fb7
yajamon <yajamon.tatsuki@gmail.com>
2017-10-21 03:57:02 +0900

    Edit TicketMaker: Singletonise

--- src/singleton/ticketMaker.ts
+++ src/singleton/ticketMaker.ts
@@ -1,5 +1,15 @@
 export class TicketMaker {
+    public static getInstance(): TicketMaker {
+        if (!this._instance) {
+            this._instance = new TicketMaker();
+        }
+        return this._instance;
+    }
+    private static _instance: TicketMaker;
     private ticket: number = 1000;
+
+    private constructor() {}
+
     public getNextTicketNumber() {
         this.ticket++;
         return this.ticket;
