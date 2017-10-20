d8dc7a3e7db233f170e9a49a3d7140f6cea25ec8
yajamon <yajamon.tatsuki@gmail.com>
2017-10-21 00:59:33 +0900

    Fix typo

--- src/factoryMethod/idCard.ts
+++ src/factoryMethod/idCard.ts
@@ -4,7 +4,7 @@ import { Product } from "./product";
 export class IDCard extends Product {
     constructor(public readonly owner: string) {
         super();
-        process.stdout.write(`${this.owner}のカードを使います。\n`);
+        process.stdout.write(`${this.owner}のカードを作ります。\n`);
     }
     public use() {
         process.stdout.write(`${this.owner}のカードを使います。\n`);
