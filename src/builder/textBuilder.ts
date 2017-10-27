84df4ba9e3715408142a361a745ad50d2b74a298
yajamon <yajamon.tatsuki@gmail.com>
2017-10-27 18:33:46 +0900

    Add class: TextBuilder

---
+++ src/builder/textBuilder.ts
@@ -0,0 +1,29 @@
+import { Builder } from "./builder";
+
+export class TextBuilder extends Builder {
+    private buffer: string;
+
+    public makeTitle(title: string) {
+        this.buffer += "==========================\n";
+        this.buffer += `「${title}」\n`;
+        this.buffer += "\n";
+    }
+
+    public makeString(str: string) {
+        this.buffer += `■${str}\n`;
+        this.buffer += "\n";
+    }
+
+    public makeItems(texts: string[]) {
+        this.buffer += texts.map((text) => `　・${text}\n`).join("");
+        this.buffer += "\n";
+    }
+
+    public close() {
+        this.buffer += "==========================\n";
+    }
+
+    public getResult(): string {
+        return this.buffer;
+    }
+}
